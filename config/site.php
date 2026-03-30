558217b48c5b475c157ab26513e30c2c31cf4a06
Gevorg Tadevosyan <136929443+ArmChegevara@users.noreply.github.com>
2026-03-30 20:26:37 +0200

    add numero du telephone

--- config/site.php
+++ config/site.php
@@ -2,7 +2,7 @@
 
 return [
     'site_name' => 'NeuraScale',
-    'phone' => '+33 6 00 00 00 00',
+    'phone' => '+33 7 51 33 41 81',
     'email' => 'contact@neurascale.fr',
     'city' => 'Reims',
     'tagline' => 'Création de sites web, automatisation et solutions digitales professionnelles',
